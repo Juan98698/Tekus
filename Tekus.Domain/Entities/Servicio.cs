2e9514f31549ec5bdf879e16744175fd01319272
JuanManuel <juancardona321829@correo.itm.edu.co>
2025-12-13 00:15:21 -0500

    Creacion de las entidades

---
+++ Tekus.Domain/Entities/Servicio.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tekus.Domain.ValueObjects;
+
+namespace Tekus.Domain.Entities
+{
+    public class Servicio
+    {
+        public int Id { get; private set; }
+
+        public string Nombre { get; private set; }
+        public decimal ValorHoraUsd { get; private set; }
+
+        private readonly List<Pais> _paises = new();
+        public IReadOnlyCollection<Pais> Paises => _paises.AsReadOnly();
+
+        public Servicio(string nombre, decimal valorHoraUsd)
+        {
+            SetNombre(nombre);
+            SetValorHoraUsd(valorHoraUsd);
+        }
+
+        private void SetNombre(string nombre)
+        {
+            if (string.IsNullOrWhiteSpace(nombre))
+                throw new ArgumentException("El nombre del servicio es obligatorio");
+
+            Nombre = nombre.Trim();
+        }
+
+        private void SetValorHoraUsd(decimal valorHoraUsd)
+        {
+            if (valorHoraUsd <= 0)
+                throw new ArgumentException("El valor por hora debe ser mayor a cero");
+
+            ValorHoraUsd = valorHoraUsd;
+        }
+
+        public void Actualizar(string nombre, decimal valorHoraUsd)
+        {
+            SetNombre(nombre);
+            SetValorHoraUsd(valorHoraUsd);
+        }
+
+        public void AgregarPais(Pais pais)
+        {
+            if (_paises.Any(p => p.Equals(pais)))
+                throw new ArgumentException("El país ya está asociado al servicio");
+
+            _paises.Add(pais);
+        }
+
+        public void RemoverPais(Pais pais)
+        {
+            _paises.Remove(pais);
+        }
+
+      
+                
+
+
+
+
+
+    }
+}
---
+++ Tekus.Domain/ValueObjects/Pais.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tekus.Domain.ValueObjects
+{
+    public class Pais
+    {
+        public string Codigo { get; }
+        public string Nombre { get; }
+
+        public Pais(string codigo, string nombre)
+        {
+            if (string.IsNullOrWhiteSpace(codigo))
+                throw new ArgumentException("El código del país es obligatorio");
+
+            if (string.IsNullOrWhiteSpace(nombre))
+                throw new ArgumentException("El nombre del país es obligatorio");
+
+            Codigo = codigo.Trim().ToUpper();
+            Nombre = nombre.Trim();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is not Pais other)
+                return false;
+
+            return Codigo == other.Codigo;
+        }
+
+        public override int GetHashCode()
+        {
+            return Codigo.GetHashCode();
+        }
+
+        
+
+
+
+    }
+
+}
